93a3260823f9e00e6299909a9c8d2819e2eb676a
Sebastian Pieniążek <severius5@gmail.com>
2017-02-26 18:52:38 +0100

    Added admin panel

---
+++ src/BazaSmyczy/Controllers/AdminPanelController.cs
@@ -0,0 +1,19 @@
+﻿using BazaSmyczy.Core.Consts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BazaSmyczy.Controllers
+{
+    [Authorize(Roles = Roles.Administrator)]
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
