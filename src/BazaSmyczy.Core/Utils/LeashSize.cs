4d046ca117504919c535148b8c6143c9ce151284
Sebastian Pieniążek <severius5@gmail.com>
2017-03-18 20:02:53 +0100

    Added new leash size

--- src/BazaSmyczy.Core/Utils/LeashSize.cs
+++ src/BazaSmyczy.Core/Utils/LeashSize.cs
@@ -1,7 +1,11 @@
-﻿namespace BazaSmyczy.Core.Utils
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BazaSmyczy.Core.Utils
 {
     public enum LeashSize
     {
+        [Display(Name = "Very Small")]
+        VerySmall = 5,
         Small = 10,
         Medium = 20,
         Large = 30
