c2552755ed08aaa7f0a7a9c2e37c9fba95f4bbf4
Sebastian Pieniążek <severius5@gmail.com>
2017-03-18 20:05:05 +0100

    Display total count of leashes in base/search results

--- src/BazaSmyczy.Core/Utils/PaginatedList.cs
+++ src/BazaSmyczy.Core/Utils/PaginatedList.cs
@@ -10,10 +10,12 @@ namespace BazaSmyczy.Core.Utils
     {
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
+        public int TotalItems { get; private set; }
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
             PageIndex = pageIndex;
+            TotalItems = count;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
 
             AddRange(items);
